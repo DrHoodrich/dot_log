5058fca85703ee37498012cdcebda524c1586755
unknown <bjfrost2@alaska.edu>
2015-10-08 21:36:25 -0800

    Adding in the SQL for Regions, Districts, and Airports.

---
+++ data/districtsTable.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  File created - Thursday-October-08-2015   
+--------------------------------------------------------
+REM INSERTING into SYSTEM.DL_DISTRICTS
+SET DEFINE OFF;
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (0,'Western',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (1,'Tok',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (2,'Tazlina',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (3,'Dalton',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (4,'Fairbanks',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (5,'Valdez',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (6,'Denali',1,0);
+Insert into SYSTEM.DL_DISTRICTS (DISTRICT_ID,DISTRICT_NAME,ENABLED,REGION_ID) values (7,'Interior',1,0);
