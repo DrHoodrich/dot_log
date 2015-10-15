7153c8d25371d75c839e6b3b3c711df8956356c3
unknown <bjfrost2@alaska.edu>
2015-10-15 13:20:45 -0800

    Added in the actual airport names and insert for admin.

---
+++ data/usersTable.sql
@@ -0,0 +1,6 @@
+--------------------------------------------------------
+--  File created - Thursday-October-15-2015   
+--------------------------------------------------------
+REM INSERTING into SYSTEM.DL_USERS
+SET DEFINE OFF;
+Insert into SYSTEM.DL_USERS (USERNAME,FIRST_NAME,LAST_NAME,FAA_CODE,USER_PERMISSIONS,ENABLED,EMAIL_ADDR,REGION_MANAGER,DISTRICT_MANAGER) values ('administrator','initial','user','OTZ',1,1,'admin@example.com',1,1);
